f720f5f46fcd2d345b8d9d94cd48622bda874de2
computer123-lab <67217068+computer123-lab@users.noreply.github.com>
2021-04-21 13:20:39 +0400

    Add files via upload

---
+++ CPPHeaders/format.h
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+
+std::string StringToHex(std::string PreString) {
+  std::string iString = PreString, hString;
+  int iTemp = 0;
+  std::stringstream is, hs;
+
+  is << iString; 
+  is >> iTemp; // convert string to int
+  hs << std::hex << iTemp; // push int through hex manipulator
+  hString = hs.str(); // store hex string
+  
+  return hString;
+}
+
+void STHDriverInit() {
+	std::string DMI;
+	std::cout << "Decimal Input: ";
+	std::cin >> DMI;
+	
+	std::string STHContainer;
+	
+	STHContainer = StringToHex(DMI);
+	
+	std::cout << STHContainer << "\n";
+	
+}
+
+std::string LittleEndian(std::string InputHex) {
+    int IHLen;
+	IHLen = InputHex.size();
+	int LittleEndianSectors = IHLen / 2;
+	std::string LEOutput;
+	int TickerClockLE = LittleEndianSectors;
+	
+	if (LittleEndianSectors == 1) {
+		std::string blob = "";
+	}
+	else {
+		while (TickClockLE > 0) {
+			
+		}
+	}
+	return "pop";
+}
